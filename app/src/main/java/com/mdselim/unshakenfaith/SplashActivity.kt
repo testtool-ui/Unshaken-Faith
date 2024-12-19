158511584b586be8938a0723be66f025cb3e18bf
testtool-ui <godlikegamer9394@gmail.com>
2024-12-19 22:22:32 +0530

    Update SplashActivity.kt

--- app/src/main/java/com/mdselim/unshakenfaith/SplashActivity.kt
+++ app/src/main/java/com/mdselim/unshakenfaith/SplashActivity.kt
@@ -2,20 +2,12 @@ package com.mdselim.unshakenfaith
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-
-        // Delay for 2 seconds before transitioning to the MainActivity
-        Handler(Looper.getMainLooper()).postDelayed({
-    val intent = Intent(this, MainActivity::class.java)
-    startActivity(intent)
-    finish() // Close SplashActivity
-    }, 2000) // 2000ms = 2 seconds
+        super.onCreate(savedInstanceState())
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
