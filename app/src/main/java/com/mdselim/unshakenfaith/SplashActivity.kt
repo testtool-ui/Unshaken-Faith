0ecd26c0270e70695dc0b9207320a0acd07a7f88
testtool-ui <godlikegamer9394@gmail.com>
2024-12-19 22:27:44 +0530

    Update SplashActivity.kt

--- app/src/main/java/com/mdselim/unshakenfaith/SplashActivity.kt
+++ app/src/main/java/com/mdselim/unshakenfaith/SplashActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState())
+    override fun onCreate(savedInstanceState: Bundle?) { // Correct signature
+        super.onCreate(savedInstanceState) // Correct invocation
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
